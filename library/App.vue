00a7ff9be95c1cc85141831e7ab0f87562e3e6e2
Clément Jacquemaire <clement@unifai.fr>
2023-11-24 22:38:21 +0100

    UPDATE config

---
+++ library/App.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import LineupFilter from './src/LineupFilter.vue'
+import { Filters } from './interfaces/filter'
+
+const filters = ref<Filters>([
+    {
+      id: 'status',
+      label: 'Statut',
+      type: 'list',
+      icon: 'circle',
+      options: [
+        {
+          label: 'En traitement...',
+          icon: 'circle',
+        },
+      ],
+    },
+    {
+      id: 'name',
+      label: 'Nom',
+      type: 'text',
+      icon: 'search',
+    },
+    {
+      id: 'creation_date',
+      label: 'Date de création',
+      type: 'date',
+      icon: 'calendar',
+    },
+    {
+      id: 'last_update_date',
+      label: 'Date de modification',
+      type: 'date',
+      icon: 'calendar',
+    },
+
+  ])
+
+const data= ref([])
+</script>
+
+<template>
+  <LineupFilter
+    :filters="filters"
+    :data="data"
+  />
+</template>
+
+<style>
+</style>
--- library/vite.config.ts
+++ library/vite.config.ts
@@ -1,28 +1,32 @@
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import checker from 'vite-plugin-checker';
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), checker({ typescript: true })],
   build: {
     lib: {
       // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, 'src/main.ts'),
-      name: 'lineup-filter',
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "lineup-filter",
       // the proper extensions will be added
-      fileName: 'lineup-filter',
+      fileName: "lineup-filter",
     },
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ['vue'],
+      input: {
+        main: resolve(__dirname, "src/index.ts"),
+      },
+      external: ["vue"],
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
         globals: {
-          vue: 'Vue',
+          vue: "Vue",
         },
       },
     },
   },
-})
+});
